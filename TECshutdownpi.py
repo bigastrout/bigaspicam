306003eae8228abf3492a489a063e1e9b1ba3e45
bigastrout <bigastrout@gmail.com>
2015-01-18 04:03:09 -0800

    Create TECshutdownpi.py

---
+++ TECshutdownpi.py
@@ -0,0 +1,21 @@
+# Shuts rpi down safely with button press
+# 
+import RPi.GPIO as GPIO
+import time
+import os
+ 
+# Use the Broadcom SOC Pin numbers not board numbers
+# Setup the Pin with Internal pullups enabled and PIN in reading mode.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+ 
+# Our function on what to do when the button is pressed
+def Shutdown(channel):
+ os.system("sudo shutdown -h now")
+ 
+# Add our function to execute when the button pressed event happens
+GPIO.add_event_detect(17, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
+ 
+# Now wait!
+while 1:
+ time.sleep(1)
